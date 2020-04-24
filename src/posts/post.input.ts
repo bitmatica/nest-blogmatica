e334233cc440afe07a37467549314d13b42ddfdb
Zac Palin <zpalin@gmail.com>
2020-04-24 11:42:46 -0700

    Adds support for CLI plugin

---
+++ src/posts/post.input.ts
@@ -0,0 +1,11 @@
+import { Field, InputType, ObjectType, OmitType, PartialType } from '@nestjs/graphql';
+import { MutationResponse } from '../common/types';
+import { Post } from './post.entity'
+
+@InputType()
+export class PostInput extends PartialType(OmitType(Post, ['id']), InputType) {}
+
+@ObjectType()
+export class PostMutationResponse extends MutationResponse {
+  post?: Post
+}
--- src/posts/post.resolver.ts
+++ src/posts/post.resolver.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { DeletionResponse } from '../common/types';
 import { Post } from './post.entity';
-import { PostInput, PostMutationResponse } from './types';
+import { PostInput, PostMutationResponse } from './post.input';
 
 @Resolver(of => Post)
 export class PostsResolver {
