e68e9fcd2f6c208c93f7696a3291ce9f40b4ff76
Zac Palin <zpalin@gmail.com>
2020-04-27 16:17:16 -0700

    POC override resolver

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,10 +1,15 @@
-import { Resolver } from '@nestjs/graphql'
+import { Type } from '@nestjs/common'
+import { InputType, OmitType, Resolver } from '@nestjs/graphql'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { FAKE_CURRENT_USER } from '../core/can'
 import { Create, CreateModelArgs, CreateModelResolver, defaultCreateModelMutation } from '../core/resolvers/actions'
 import { BaseModelResolver } from '../core/resolvers/model'
 import { ICreateModelInput, IMutationResponse } from '../core/resolvers/types'
 import { Post } from './post.entity'
+
+@InputType()
+class CreatePostInput extends OmitType(Post as Type<any>, [ 'id', 'created', 'updatedAt', 'authorId', 'authors' ], InputType) {}
 
 @Resolver(of => Post)
 export class PostsResolver extends BaseModelResolver(Post, { without: [ Create ] }) {
@@ -13,6 +18,11 @@ export class PostsResolver extends BaseModelResolver(Post, { without: [ Create ]
 
   @CreateModelResolver(Post)
   async create(@CreateModelArgs(Post) input: ICreateModelInput<Post>): Promise<IMutationResponse<Post>> {
-    return defaultCreateModelMutation(Post, this.repo, input)
+    const user = FAKE_CURRENT_USER
+    const modifiedInput: ICreateModelInput<Post> = {
+      ...input,
+      authorId: user.id,
+    }
+    return defaultCreateModelMutation(Post, this.repo, modifiedInput)
   }
 }
