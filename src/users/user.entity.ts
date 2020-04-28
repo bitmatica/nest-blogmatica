045ed2fd8ac68611b043a0665881f1abb541b04f
Kasey Moffat <kaseymoffat@Kaseys-MacBook-Pro.local>
2020-04-28 15:34:50 -0700

    support login/logout/whoAmI and currentUser decorator

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,9 @@
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, HideField, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, OneToMany } from 'typeorm'
 import { ActionScope, Can, RecordScope, UserScope } from '../core/can'
 import { BaseModel } from '../core/model'
 import { Post } from '../posts/post.entity'
+import * as bcrypt from 'bcrypt'
 
 @ObjectType()
 @Entity()
@@ -15,10 +16,23 @@ import { Post } from '../posts/post.entity'
 })
 export class User extends BaseModel {
   @Field()
-  @Column()
+  @Column({ unique: true })
   email: string
 
   @Field(type => [ Post ])
   @OneToMany(type => Post, post => post.author, { lazy: true })
   posts: Promise<Array<Post>>
+
+  @HideField()
+  @Column()
+  passwordHash: string
+
+  async setPassword(password: string): Promise<void> {
+    const salt = await bcrypt.genSalt(10)
+    this.passwordHash = await bcrypt.hash(password, salt)
+  }
+
+  async checkPassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.passwordHash)
+  }
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,129 @@
-import { Resolver } from '@nestjs/graphql'
-import { BaseModelResolver } from '../core/resolvers/model'
-import { User } from './user.entity'
+import { Args, Context, Field, InputType, Mutation, ObjectType, Query, Resolver } from '@nestjs/graphql';
+import { BaseModelResolver } from '../core/resolvers/model';
+import { User } from './user.entity';
+import { MutationResponse } from '../core/resolvers/types';
+import { clearTokenCookie, generateTokenForUserId, setTokenCookie } from './authentication';
+import { Create } from '../core/resolvers/actions';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { IContext } from '../common/context';
+import { CurrentUser } from '../decorators/currentUser';
+
+@ObjectType()
+export class UserMutationResponse extends MutationResponse<User> {
+  @Field({nullable: true})
+  user?: User
+}
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  email: string
+
+  @Field()
+  password: string
+}
+
+@InputType()
+export class UserLoginArgs {
+  @Field()
+  email: string
+
+  @Field()
+  password: string
+}
+
+@ObjectType()
+export class UserLoginResponse extends UserMutationResponse {
+  @Field({nullable: true})
+  token?: string
+}
 
 @Resolver()
-export class UsersResolver extends BaseModelResolver(User) {}
+export class UsersResolver extends BaseModelResolver(User, { without: [ Create ] }) {
+  @InjectRepository(User)
+  protected repo: Repository<User>
+
+  @Mutation(returns => UserMutationResponse)
+  async createUser(
+    @Args('input', { type: () => CreateUserInput }) input: CreateUserInput
+  ) {
+    try {
+      const model = new User()
+      model.email = input.email
+      await model.setPassword(input.password)
+      const saved = await this.repo.save(model)
+      return {
+        success: true,
+        message: `User created.`,
+        user: saved,
+      }
+    } catch (err) {
+      return {
+        success: false,
+        message: err.message,
+      }
+    }
+  }
+
+  @Mutation(returns => UserLoginResponse!)
+  async login(
+    @Args('input', { type: () => UserLoginArgs }) input: UserLoginArgs,
+    @Context() context: IContext
+  ) {
+    try {
+      const user: User | undefined = await this.repo.findOne({ email: input.email })
+      if (!user) {
+        return {
+          success: false,
+          message: 'User does not exist'
+        }
+      }
+      const correctPassword = await user.checkPassword(input.password)
+      if (!correctPassword) {
+        return {
+          success: false,
+          message: 'Incorrect password'
+        }
+      }
+      const token = await generateTokenForUserId(user.id)
+      setTokenCookie(context.res, token)
+      return {
+        success: true,
+        message: 'Login successful!',
+        user,
+        token: token
+      }
+    } catch(err) {
+      return {
+        success: false,
+        message: 'Login failed'
+      }
+    }
+  }
+
+  @Mutation(returns => UserMutationResponse!)
+  async logout(
+    @Context() context: IContext
+  ) {
+    try {
+      clearTokenCookie(context.res)
+      return {
+        success: true,
+        message: 'Logout successful!'
+      }
+    } catch(err) {
+      return {
+        success: false,
+        message: 'Logout failed'
+      }
+    }
+  }
+
+  @Query(returns => User, { nullable: true })
+  async whoAmI(
+    @CurrentUser() user: User
+  ) {
+    return user
+  }
+}
