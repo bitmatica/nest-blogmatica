29299fd101dc4883f7dfcabb0a7922588aba3e42
Kasey Moffat <kaseymoffat@Kaseys-MacBook-Pro.local>
2020-04-29 17:31:53 -0700

    add login link

--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -7,15 +7,37 @@ class AccessTokenResponse {
   refresh_token: string
 }
 
+const CLIENT_ID = 'CLIENT_ID_OVERRIDE_ME'
+const CLIENT_SECRET = 'CLIENT_SECRET_OVERRIDE_ME'
+const REDIRECT_URI = 'http://gusto.apps.bitmatica.com/authCallback'
+const REDIRECT_URI_ESCAPED = 'http:%2F%2Fgusto.apps.bitmatica.com%2FauthCallback'
+
 @Controller()
 export class OauthController {
   constructor(private httpService: HttpService) {}
+
+  @Get('gustoLogin')
+  root() {
+    return `<html><a href="https://api.gusto.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI_ESCAPED}&response_type=code">Authorize with Gusto</a></html>`
+  }
 
   @Get('authCallback')
   async authCallback(
     @Query('code') code: string,
   ) {
     console.log('code: ' + code)
+    const config = {
+      url: 'https://api.gusto.com/oauth/token',
+      params: {
+        code,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+        grant_type: 'authorization_code'
+      }
+    }
+    console.log(this.httpService.axiosRef.getUri(config))
+
     const result = await this.httpService.post(
       'https://api.gusto.com/oauth/token',
       {},
@@ -29,6 +51,7 @@ export class OauthController {
         }
       }
     ).toPromise()
+
     console.log(result)
     // return result.data
   }
