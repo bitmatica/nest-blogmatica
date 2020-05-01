7576fad4e07b1127a949a5e61ed4dce4e1c693eb
Zac Palin <zpalin@gmail.com>
2020-05-01 12:12:27 -0700

    Converts Can to class

--- src/core/can/scopes/user.ts
+++ src/core/can/scopes/user.ts
@@ -1,4 +1,13 @@
+import { IUser } from '../../context'
+
 export enum UserScope {
   Anyone,
   Authenticated,
+}
+
+export function getUserScopes(user: IUser | undefined): Array<UserScope> {
+  if (!user) {
+    return [ UserScope.Anyone ]
+  }
+  return [ UserScope.Anyone, UserScope.Authenticated ]
 }
