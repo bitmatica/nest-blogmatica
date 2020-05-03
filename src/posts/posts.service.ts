b5760fa0ab3d8305319c84a6981e9cb9baceaf5f
Zac Palin <zpalin@gmail.com>
2020-05-03 15:14:14 -0700

    Override service method poc

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { GraphQLResolveInfo } from 'graphql'
+import { IContext } from '../core/context'
 import { BaseModelService } from '../core/service/model'
 import { Post } from './post.entity'
 
 @Injectable()
-export class PostsService extends BaseModelService(Post) {}
+export class PostsService extends BaseModelService(Post) {
+  list(
+    context: IContext,
+    info: GraphQLResolveInfo,
+  ): Promise<Array<Post>> | Array<Post> {
+    console.log('Posts List Override')
+    return super.list(context, info)
+  }
+}
