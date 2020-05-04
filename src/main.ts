0262cef463858ac61d7a704cf5ae5cd14671b41f
Zac Palin <zpalin@gmail.com>
2020-05-03 21:01:49 -0700

    Adds typescript stack traces

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core'
+import sourceMapSupport from 'source-map-support'
 import { AppModule } from './app.module'
+
+sourceMapSupport.install()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
