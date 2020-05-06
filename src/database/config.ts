5a3a34714730d20f81c9056761787233d3a810a9
Kasey Moffat <kasey@bitmatica.com>
2020-05-05 17:23:39 -0700

    local db config

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,5 +1,27 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface'
 
+// const databaseConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'blogmatica',
+//   password: 'blogmatica_password',
+//   database: 'blogmatica',
+//   synchronize: false,
+//   migrationsRun: false,
+//   logging: true,
+//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//   subscribers: [__dirname + '/../**/*.subscriber.{js,ts}'],
+//   migrations: [__dirname + '/migrations/*.{js,ts}'],
+//   cli: {
+//     entitiesDir: 'src/**/models',
+//     migrationsDir: 'src/database/migrations',
+//   },
+//   extra: {
+//     connectionLimit: 5,
+//   },
+// }
+
 const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'ec2-54-86-170-8.compute-1.amazonaws.com',
