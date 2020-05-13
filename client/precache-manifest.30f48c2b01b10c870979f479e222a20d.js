245d54111e5a0a898e3b22818152016887003e0a
Kasey Moffat <kasey@bitmatica.com>
2020-05-12 18:18:59 -0700

    add client

---
+++ client/precache-manifest.30f48c2b01b10c870979f479e222a20d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d281d2136c79c0bd926e8f35ced4db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b586ec3da0b0ee12223",
+    "url": "/static/js/2.21d98cd0.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.21d98cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85592d489cd4e07091f7",
+    "url": "/static/js/main.740e760b.chunk.js"
+  },
+  {
+    "revision": "793e89db6600170924a6",
+    "url": "/static/js/runtime-main.2a741048.js"
+  }
+]);
