44c284465accc53e68be1a73e7484e0373e71bd4
Kasey Moffat <kasey@bitmatica.com>
2020-05-13 18:23:58 -0700

    add db config file

---
+++ src/config/database.ts
@@ -0,0 +1,23 @@
+import { ConfigService } from '@nestjs/config'
+
+export const databaseFactory = async (configService: ConfigService) => ({
+  type: 'postgres' as 'postgres',
+  host: configService.get('DATABASE_HOST', 'localhost'),
+  port: configService.get<number>('DATABASE_PORT', 5432),
+  username: configService.get('DATABASE_USER', 'blogmatica'),
+  password: configService.get('DATABASE_PASS', 'blogmatica_password'),
+  database: configService.get('DATABASE_DB', 'blogmatica'),
+  synchronize: false,
+  migrationsRun: false,
+  logging: true,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  subscribers: [__dirname + '/../**/*.subscriber.{js,ts}'],
+  migrations: [__dirname + '/../database/migrations/*.{js,ts}'],
+  cli: {
+    entitiesDir: 'src/**/models',
+    migrationsDir: 'src/database/migrations',
+  },
+  extra: {
+    connectionLimit: 5,
+  },
+})
