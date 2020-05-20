445396ae8026b4bc20c0d38549dd6b073fe2d617
Zac Palin <zpalin@gmail.com>
2020-05-20 09:04:18 -0700

    npm -> yarn, node version 14.1 -> lts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common'
-import { GraphQLModule } from '@nestjs/graphql'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { PostsModule } from './posts/posts.module'
-import { UsersModule } from './users/users.module'
-import { CommentsModule } from './comments/comments.module'
-import { OAuthModule } from './oauth/oauth.module'
-import { GustoModule } from './gusto/gusto.module'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { join } from 'path'
-import oauthConfig from './config/oauthConfig'
-import { UsersService } from './users/users.service'
-import { graphqlConfigFactory } from './config/graphqlConfigFactory'
+import { CommentsModule } from './comments/comments.module'
 import databaseConfig from './config/databaseConfig'
 import graphqlConfig from './config/graphqlConfig'
+import { graphqlConfigFactory } from './config/graphqlConfigFactory'
+import oauthConfig from './config/oauthConfig'
+import { getOrThrow } from './core/utils'
+import { GustoModule } from './gusto/gusto.module'
+import { OAuthModule } from './oauth/oauth.module'
+import { PostsModule } from './posts/posts.module'
+import { UsersModule } from './users/users.module'
+import { UsersService } from './users/users.service'
 
 @Module({
   imports: [
@@ -28,7 +29,8 @@ import graphqlConfig from './config/graphqlConfig'
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => configService.get('database'),
+      useFactory: async (configService: ConfigService) =>
+        getOrThrow(configService.get('database'), 'Database config was not found'),
     }),
     GraphQLModule.forRootAsync({
       imports: [ConfigModule, UsersModule],
